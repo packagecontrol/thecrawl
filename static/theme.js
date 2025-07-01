5c920e4a2488a671d1a509507e0c9deb88b283dd
Koen Lageveen <koen.lageveen@me.com>
2025-07-01 20:45:07 +0200

    if user didn't express preference follow system

--- static/theme.js
+++ static/theme.js
@@ -1,32 +1,34 @@
-const system_pref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const system_pref = window.matchMedia('(prefers-color-scheme: dark)');
+const system_theme = system_pref.matches ? 'dark' : 'light';
 const user_pref = localStorage.getItem('theme');
 
-let current_theme = user_pref ?? system_pref;
+let current_theme = user_pref ?? system_theme;
 
 document.documentElement.setAttribute('data-theme', current_theme);
 
-function toggle() {
+function toggle(user_initiated) {
   if (current_theme === 'light') {
     current_theme = 'dark';
-    if (system_pref === 'dark') {
-      localStorage.removeItem('theme');
-    } else {
-      localStorage.setItem('theme', 'dark');
-    }
   } else {
     current_theme = 'light';
-    if (system_pref === 'light') {
-      localStorage.removeItem('theme');
-    } else {
-      localStorage.setItem('theme', 'light');
-    }
   }
 
+  if (user_initiated) {
+    localStorage.setItem('theme', current_theme);
+  }
   document.documentElement.setAttribute('data-theme', current_theme);
 }
 
 document.querySelector('#theme-toggle').onclick = (event) => {
   event.preventDefault();
   event.stopPropagation();
-  toggle();
+  toggle(true);
 };
+
+system_pref.addEventListener('change', () => {
+  if (localStorage.getItem('theme')) {
+    // we have a user pref, so don't switch;
+    return;
+  }
+  toggle(false);
+});
