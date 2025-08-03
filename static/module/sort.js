eb2f484be8d1dff253f69ab0d7bc87c253e3c796
Koen Lageveen <koen.lageveen@me.com>
2025-08-03 18:31:56 +0200

    add sort by installs

--- static/module/sort.js
+++ static/module/sort.js
@@ -9,18 +9,25 @@ export class Sort {
       case 'name-desc':
         return sortedPackages.sort((a, b) => b.name.toLowerCase().localeCompare(a.name.toLowerCase()))
 
+      case 'installed':
+        return sortedPackages.sort((a, b) => {
+          const A = parseInt(a.installed) || 0
+          const B = parseInt(b.installed) || 0
+          return B - A // High to low
+        })
+
       case 'stars':
         return sortedPackages.sort((a, b) => {
-          const starsA = parseInt(a.stars) || 0
-          const starsB = parseInt(b.stars) || 0
-          return starsB - starsA // High to low
+          const A = parseInt(a.stars) || 0
+          const B = parseInt(b.stars) || 0
+          return B - A // High to low
         })
 
       case 'stars-desc':
         return sortedPackages.sort((a, b) => {
-          const starsA = parseInt(a.stars) || 0
-          const starsB = parseInt(b.stars) || 0
-          return starsA - starsB // Low to high
+          const A = parseInt(a.stars) || 0
+          const B = parseInt(b.stars) || 0
+          return A - B // Low to high
         })
 
       case 'author':
