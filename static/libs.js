52bdceb01cad8bee784992adc50360534e091d19
Koen Lageveen <koen.lageveen@me.com>
2025-10-01 16:35:30 +0200

    reuse in-page-search across labels and libs

--- static/libs.js
+++ static/libs.js
@@ -1,17 +1,17 @@
 import minisearch from 'https://cdn.jsdelivr.net/npm/minisearch@7.1.2/+esm'
-import { Search } from './module/search.js'
+import { SimpleSearch } from './module/simplesearch.js'
 
 /**
  * Simplified search features on the library listing page.
  */
 
 const data = []
-const cards = document.querySelectorAll('[data-lib-name]')
+const cards = document.querySelectorAll('[data-name]')
 cards.forEach((card) => {
   data.push({
-    name: card.dataset.libName,
-    author: card.dataset.libAuthor,
-    description: card.dataset.libDescription,
+    name: card.dataset.name,
+    author: card.dataset.author,
+    description: card.dataset.description,
   })
 })
 
@@ -27,54 +27,5 @@ const minisrch = new minisearch({
 })
 minisrch.addAll(data)
 
-const search = new Search(minisrch)
-const form = document.forms.search
-const input = form.elements['lib']
-const handleInput = () => {
-  const query = input.value.toLowerCase().trim()
-  const heading = document.querySelector('[data-list-target="heading"]')
-
-  if (query === '') {
-    cards.forEach((card) => {
-      card.closest('li').style.display = null
-    })
-    heading.innerText = 'List'
-    return
-  }
-
-  const names = search.search(query).map(result => result.name)
-  cards.forEach((card) => {
-    if (names.indexOf(card.dataset.libName) < 0) {
-      card.closest('li').style.display = 'none'
-    }
-    else {
-      card.closest('li').style.display = null
-    }
-    if (names.length === 1) {
-      heading.innerText = '1 Result'
-    }
-    else {
-      heading.innerText = names.length + ' Results'
-    }
-  })
-}
-
-let debounceTimeout
-
-// Handle form submission
-input.form.onsubmit = (event) => {
-  event.preventDefault()
-  event.stopPropagation()
-  clearTimeout(debounceTimeout)
-
-  handleInput()
-}
-
-// Handle input changes (search as you type)
-input.addEventListener('input', () => {
-  clearTimeout(debounceTimeout)
-
-  debounceTimeout = setTimeout(() => {
-    handleInput()
-  }, 300) // .3 seconds
-})
+const search = new SimpleSearch(minisrch, cards, document.getElementById('search-field'))
+search.init()
---
+++ static/module/simplesearch.js
@@ -0,0 +1,66 @@
+import { Search } from './search.js'
+
+export class SimpleSearch {
+  search = null
+  input = null
+  heading = document.querySelector('[data-list-target="heading"]')
+  cards = []
+
+  constructor(minisearch, cards, input) {
+    this.search = new Search(minisearch)
+    this.input = input
+    this.cards = cards
+  }
+
+  handleInput() {
+    const query = this.input.value.toLowerCase().trim()
+
+    if (query === '') {
+      this.cards.forEach((card) => {
+        card.closest('li').style.display = null
+      })
+      this.heading.innerText = 'List'
+      return
+    }
+
+    const results = this.search.search(query).map(result => result.name)
+
+    if (results.length === 1) {
+      this.heading.innerText = '1 Result'
+    }
+    else {
+      this.heading.innerText = results.length + ' Results'
+    }
+
+    this.cards.forEach((card) => {
+      if (results.indexOf(card.dataset.name) < 0) {
+        card.closest('li').style.display = 'none'
+      }
+      else {
+        card.closest('li').style.display = null
+      }
+    })
+  }
+
+  init() {
+    let debounceTimeout
+
+    // Handle form submission
+    this.input.form.onsubmit = (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      clearTimeout(debounceTimeout)
+
+      this.handleInput()
+    }
+
+    // Handle input changes (search as you type)
+    this.input.addEventListener('input', () => {
+      clearTimeout(debounceTimeout)
+
+      debounceTimeout = setTimeout(() => {
+        this.handleInput()
+      }, 300) // .3 seconds
+    })
+  }
+}
