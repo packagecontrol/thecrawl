5d86e544217c48f8bb5ef69ee268829b8a28bcd3
Koen Lageveen <koen.lageveen@me.com>
2025-10-12 17:34:23 +0200

    reusable toast

--- static/about.js
+++ static/about.js
@@ -1,15 +1,13 @@
+import { Toast } from './module/toast.js'
+
 document.querySelectorAll('pre').forEach((codeblock) => {
   const wrapper = document.createElement('div')
   wrapper.classList.add('clipboard-wrapper')
 
-  const toast_temp = document.querySelector('template#toast')
-  const toast = toast_temp.content.cloneNode(true)
-  const toast_el = toast.querySelector('.toast')
-  toast_el.innerText = 'Copied!'
-  document.body.appendChild(toast)
-
   const template = document.querySelector('template#clipboard-button')
   const button = template.content.cloneNode(true)
+
+  const toast = new Toast('Copied!')
 
   button.querySelector('button').onclick = (event) => {
     event.preventDefault()
@@ -17,24 +15,12 @@ document.querySelectorAll('pre').forEach((codeblock) => {
     navigator.clipboard.writeText(codeblock.innerText.trim())
     wrapper.classList.add('copied')
 
-    const target = event.target.closest('button')
-
-    toast_el.style.left = target.getBoundingClientRect().left
-      + target.getBoundingClientRect().width
-      - toast_el.getBoundingClientRect().width
-      + 'px'
-    toast_el.style.top = target.getBoundingClientRect().top
-      - toast_el.getBoundingClientRect().height
-      + 'px'
-
-    toast_el.setAttribute('aria-hidden', 'false')
+    toast.near(event.target.closest('button'))
+    toast.pop()
 
     window.setTimeout(() => {
       wrapper.classList.remove('copied')
     }, 500)
-    window.setTimeout(() => {
-      toast_el.setAttribute('aria-hidden', 'true')
-    }, 1500)
   }
 
   codeblock.insertAdjacentElement('beforebegin', wrapper)
---
+++ static/module/toast.js
@@ -0,0 +1,33 @@
+export class Toast {
+  constructor(text) {
+    const toast_temp = document.querySelector('template#toast')
+    const instance = toast_temp.content.cloneNode(true)
+    this.element = instance.querySelector('.toast')
+    this.element.innerText = text
+    document.body.appendChild(this.element)
+
+    this.timer = null
+  }
+
+  pop() {
+    this.element.setAttribute('aria-hidden', 'false')
+
+    window.clearTimeout(this.timer)
+    this.timer = window.setTimeout(() => {
+      this.element.setAttribute('aria-hidden', 'true')
+    }, 1500)
+  }
+
+  near(target) {
+    const target_rect = target.getBoundingClientRect()
+    const toast_rect = this.element.getBoundingClientRect()
+
+    this.element.style.left = target_rect.left
+      + target_rect.width
+      - toast_rect.width
+      + 'px'
+    this.element.style.top = target_rect.top
+      - toast_rect.height
+      + 'px'
+  }
+}
